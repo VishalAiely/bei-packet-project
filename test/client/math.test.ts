09749620761effe9609041e32f4adf83bb063206
Selena Xue <sxue3@vols.utk.edu>
2020-12-31 19:22:23 -0600

    added backend math tests descriptions

--- test/client/math.test.ts
+++ test/client/math.test.ts
@@ -1,3 +1,13 @@
-test.todo('Only works with POST');
+describe('Math Generator', () => {
+  it.todo('Math function gives me correct number of problems');
+  it.todo('Are the math problems random enough?');
+});
+
+describe('Single Question Generator', () => {
+  it.todo('Math function returns all neccessary fields');
+  it.todo('Math function returns the correct answers for math problems');
+  it.todo('Math function uses correct operator');
+  it.todo('Math function does not include a number bigger than the max');
+});
 
 export {};
