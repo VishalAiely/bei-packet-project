33aaac87ec528fff7afeb48e9ad50a64b44cc593
VishalAiely <vaiely@vols.utk.edu>
2021-01-17 14:28:03 -0500

    Began styling for frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import DocumentGen, { sections } from 'client/document';
-import { useRef } from 'react';
+import React, { useRef } from 'react';
 import { Difficulty, TriviaOptions } from 'utils/types/Trivia';
+import { Container, AppBar, Toolbar, Box, Paper, Chip, Button } from '@material-ui/core';
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DroppableProvided,
+  DropResult,
+  resetServerContext,
+} from 'react-beautiful-dnd';
+import 'fontsource-roboto';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
-export default function Home() {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    chipBox: {
+      display: 'flex',
+      justifyContent: 'center',
+      flexWrap: 'wrap',
+      listStyle: 'none',
+      padding: theme.spacing(0.5),
+      margin: 0,
+    },
+    chip: {
+      margin: theme.spacing(0.5),
+    },
+  })
+);
+
+export default function Home(): JSX.Element {
   const diff = useRef<HTMLSelectElement>(null);
   const triviaNums = useRef<HTMLInputElement>(null);
 
@@ -25,37 +63,99 @@ export default function Home() {
     await docs.downloadDoc();
   };
 
+  const classes = useStyles();
+
+  const [sectionData, setSectionData] = React.useState<sections[]>(['Trivia', 'Math', 'Reading']);
+
+  const handleDelete = (sectionToDelete: sections) => () => {
+    setSectionData(sections => sections.filter(section => section !== sectionToDelete));
+  };
+
+  const whenDrag = (result: DropResult) => {
+    if (!result?.destination) return;
+
+    const items = Array.from(sectionData);
+    const [newOrderitem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, newOrderitem);
+
+    setSectionData(items);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  resetServerContext();
+
   return (
-    <div className={styles.container}>
+    <React.Fragment>
       <Head>
-        <title>Create Next App</title>
+        <title>BEI Packet Generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Container maxWidth="xl">
+        <Box p={3}>
+          <AppBar position="static" color="default">
+            <Toolbar>
+              <Typography variant="h5" className={classes.title}>
+                Brain Exercise Initiative Packet Generator
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
 
-      <h2>Trivia</h2>
-      <div>
-        <p>
-          Difficulty:
-          <select ref={diff}>
-            <option value={Difficulty.Very_Easy}>Very Easy</option>
-            <option value={Difficulty.Easy}>Easy</option>
-            <option value={Difficulty.Medium}>Medium</option>
-            <option value={Difficulty.Hard}>Hard</option>
-          </select>
-        </p>
-      </div>
+        <Container maxWidth="md">
+          <DragDropContext onDragEnd={whenDrag}>
+            <Droppable droppableId="sections" direction="horizontal">
+              {(provided: DroppableProvided) => (
+                <Paper component="ul" className={classes.chipBox} ref={provided.innerRef} {...provided.droppableProps}>
+                  {sectionData.map((data, ind) => {
+                    return (
+                      <Draggable key={data} draggableId={data} index={ind}>
+                        {provided => (
+                          <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                            <Chip label={data} onDelete={handleDelete(data)} className={classes.chip} />
+                          </li>
+                        )}
+                      </Draggable>
+                    );
+                  })}
+                  {
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                    provided.placeholder
+                  }
+                </Paper>
+              )}
+            </Droppable>
+          </DragDropContext>
+          <Button
+            onClick={() => {
+              console.log(sectionData);
+            }}>
+            Check
+          </Button>
+        </Container>
 
-      <div>
-        <p>
-          Number of Questions: <input ref={triviaNums}></input>
-        </p>
-      </div>
+        {/* <Typography variant="h2">Trivia</Typography>
+        <div>
+          <p>
+            Difficulty:
+            <select ref={diff}>
+              <option value={Difficulty.Very_Easy}>Very Easy</option>
+              <option value={Difficulty.Easy}>Easy</option>
+              <option value={Difficulty.Medium}>Medium</option>
+              <option value={Difficulty.Hard}>Hard</option>
+            </select>
+          </p>
+        </div>
 
-      <main className={styles.main}>
-        <button onClick={gen}>Download</button>
-      </main>
+        <div>
+          <p>
+            Number of Questions: <input ref={triviaNums}></input>
+          </p>
+        </div>
 
-      <footer className={styles.footer}></footer>
-    </div>
+        <main>
+          <button onClick={gen}>Download</button>
+        </main> */}
+      </Container>
+    </React.Fragment>
   );
 }
