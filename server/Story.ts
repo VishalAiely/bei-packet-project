ada7be0be65db8c6b755cc2fd829efeaf06ce554
VishalAiely <vaiely@vols.utk.edu>
2021-01-21 10:24:56 -0500

    fixed file reading issues

--- server/Story.ts
+++ server/Story.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import path from 'path';
 
 export interface Story {
   title: string;
@@ -11,8 +12,11 @@ export interface Story {
 
 export type StoryCache = Record<string, Story>;
 
+const storyDataFile = path.resolve('./server', 'cache/storyData.json');
+const storyTitlesFile = path.resolve('./server', 'cache/storyTitles.json');
+
 export function getRandomStory(): Story {
-  const rawdata = fs.readFileSync('server/cache/storyData.json');
+  const rawdata = fs.readFileSync(storyDataFile);
   const parsedData = JSON.parse(rawdata.toString()) as Record<string, Story>;
   const keys = Object.keys(parsedData);
   const randomStoryIndex = Math.floor(Math.random() * keys.length);
@@ -20,20 +24,20 @@ export function getRandomStory(): Story {
 }
 
 export function getRandomStoryByCategory(category: string): Story {
-  const rawTitles = fs.readFileSync('server/cache/storyTitles.json');
+  const rawTitles = fs.readFileSync(storyTitlesFile);
   const parsedData = JSON.parse(rawTitles.toString()) as Record<string, string[]>;
   if (!parsedData[category]) throw new Error('Category Does Not Exist');
 
   const allStoriesofCategory = parsedData[category];
   const randomStory = allStoriesofCategory[Math.floor(Math.random() * allStoriesofCategory.length)];
 
-  const rawStories = fs.readFileSync('server/cache/storyData.json');
+  const rawStories = fs.readFileSync(storyDataFile);
   const parsedStories = JSON.parse(rawStories.toString()) as Record<string, Story>;
   return parsedStories[randomStory];
 }
 
 export function getStoryByName(title: string): Story {
-  const rawdata = fs.readFileSync('server/cache/storyData.json');
+  const rawdata = fs.readFileSync(storyDataFile);
   const parsedData = JSON.parse(rawdata.toString()) as Record<string, Story>;
   if (!parsedData[title]) throw new Error('Story Does not exist');
 
@@ -41,13 +45,13 @@ export function getStoryByName(title: string): Story {
 }
 
 export function getAllStoryNames(): Array<string> {
-  const rawdata = fs.readFileSync('server/cache/storyData.json');
+  const rawdata = fs.readFileSync(storyDataFile);
   const parsedData = JSON.parse(rawdata.toString()) as Record<string, Story>;
   return Object.keys(parsedData);
 }
 
 export function getAllStoriesNamesInCategory(category: string): Array<string> {
-  const rawTitles = fs.readFileSync('server/cache/storyTitles.json');
+  const rawTitles = fs.readFileSync(storyTitlesFile);
   const parsedData = JSON.parse(rawTitles.toString()) as Record<string, string[]>;
   if (!parsedData[category]) throw new Error('Category Does Not Exist');
 
@@ -55,7 +59,7 @@ export function getAllStoriesNamesInCategory(category: string): Array<string> {
 }
 
 export function getAllCategories(): Array<string> {
-  const rawdata = fs.readFileSync('server/cache/storyTitles.json');
+  const rawdata = fs.readFileSync(storyTitlesFile);
   const parsedData = JSON.parse(rawdata.toString()) as Record<string, string[]>;
   return Object.keys(parsedData);
 }
